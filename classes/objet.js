c2cd27b2dcc2751d49ae67200716fa7550da6652
Crochet Olivier <olivier.crochet@ynov.com>
2018-02-05 19:31:07 +0100

    implémentation constructeur et getter/setter de objet

---
+++ classes/objet.js
@@ -0,0 +1,30 @@
+class objet{
+
+   constructor(nom) {
+       this.nom = nom;
+       this.effet = effet;
+       this.quantite = quantite;
+
+   }
+
+   get nom(nom){
+       return this.effet;
+   }
+
+    get effet(effet){
+        return this.effet;
+    }
+    get quantite(quantite){
+        return this.effet;
+    }
+
+    set nom(nom){
+        return this.effet;
+    }
+    set effet(effet){
+        return this.effet;
+    }
+    set quantite(quantite){
+        return this.effet;
+    }
+}
