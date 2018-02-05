8d4d97c0112cba566c89e29d06d2c0029d132623
Bigeard <robin.bigeard@gmail.com>
2018-02-05 23:11:33 +0100

    ajout selection keypress

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
+var select = 1
 
-function start(){
 
-console.log('Bienvenu Sur')
-console.log('            ______   ______   ______   ______   ______    |')
-console.log('           /_____/  /_____/  /_____/  /_____/  /_____/    o')
-console.log('\n          ( PS: tkt un jour on vas trouver un nom :3 )')
-console.log('\n___________________________________________________________________')
 
-console.log('  - Comencer                  - Obtion                  - Credit')
 
+function start(select) {
+    console.clear()
+    if (select >= 4) {
+        select = 1
+    }
+    if (select <= 0) {
+        select = 3
+    }
+    document.getElementById('select').innerHTML = select
+    var chn = ' - Comencer [1]              - Obtion [2]              - Credit [3]'
+    var nouvChn = chn.replace(select, '*');
+
+    console.log('Bienvenu Sur')
+    console.log('            ______   ______   ______   ______   ______    |')
+    console.log('           /_____/  /_____/  /_____/  /_____/  /_____/    o')
+    console.log('\n          ( PS: tkt un jour on vas trouver un nom :3 )')
+    console.log('\n____________________________________________________________________')
+    console.log(nouvChn)
 }
 
 //--------------------------------- Controle Des Touche ( Detection ) -----------------------------------
@@ -17,31 +29,25 @@ document.addEventListener('keydown', (event) => {
     const keyName = event.key;
     document.getElementById('key').innerHTML = 'Keypress: ' + keyName
 
-//--------------------------------- Touche Clée ---------------------------------------------------------
+    //--------------------------------- Touche Clée ---------------------------------------------------------
 
     if (keyName == 'Enter') {
-        document.getElementById('error').style.display = 'none'
-
+        start()
     }
-    if (keyName == 'ArrowUp') {
-        document.getElementById('error').style.display = 'none'
-        
+    else if (keyName == 'ArrowUp') {
+        start(select = select + 1)
     }
-    if (keyName == 'ArrowDown') {
-        document.getElementById('error').style.display = 'none'
-        
+    else if (keyName == 'ArrowDown') {
+        start(select = select - 1)
     }
-    if (keyName == 'ArrowLeft') {
-        document.getElementById('error').style.display = 'none'
-        
+    else if (keyName == 'ArrowLeft') {
+        start(select = select + 1)
     }
-    if (keyName == 'ArrowRight') {
-        document.getElementById('error').style.display = 'none'
-        
+    else if (keyName == 'ArrowRight') {
+        start(select = select - 1)
     }
-    else{
+    else {
         document.getElementById('error').innerHTML = '~ Error ~'
         document.getElementById('error').style.display = 'block'
     }
 });
-
