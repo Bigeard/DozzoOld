fa619b104f143341a0fde9736561b4829d351d9a
Bigeard <robin.bigeard@gmail.com>
2018-02-06 18:52:44 +0100

    ajout du nom dozzo dans la console

--- main.js
+++ main.js
@@ -9,16 +9,22 @@ function menu(select) {
     console.clear()
 
     document.getElementById('select').innerHTML = select
-    var chn = ' - Commencer [1]              - Option [2]              - A propos [3]'
+    var chn = '  - Commencer [1]              - Option [2]              - A propos [3]'
    // var chn = '              - Commencer [1]              - Option [2]\n              - A propos [3]              - A propos [4]\n              - A propos [5]              - A propos [6]'
    // var chn = ' - Commencer [1]              - Option [2]              - A propos [3]\n - Commencer [4]              - Option [5]              - A propos [6]\n - Commencer [7]              - Option [8]              - A propos [9]'
     var nouvChn = chn.replace(select, '*');
 
-    console.log('Bienvenu Sur')
-    console.log('            ______   ______   ______   ______   ______    |')
-    console.log('           /_____/  /_____/  /_____/  /_____/  /_____/    o')
-    console.log('\n          ( PS: tkt un jour on vas trouver un nom :3 )')
-    console.log('\n _____________________________________________________________________')
+    console.log('          ▀█████████▄  BINVENUE DANS LE DONJON DU ...')
+    console.log('            ██▒     █▄                                        ᚐ ᚐ ᚐ  ')
+    console.log('            ██       █▄  ▒█████  ▒███████▒▒███████▒ ▒█████')
+    console.log('           ░██       ░█ ▒██▒  ██▒▒ ▒ ▒ ▄▀░▒ ▒ ▒ ▄▀░▒██▒  ██▒')
+    console.log('           ░██      ░█▀ ▒██░  ██▒░ ▒ ▄▀▒░ ░ ▒ ▄▀▒░ ▒██░  ██▒')
+    console.log('           ▒██     ░█▀░ ▒██   ██░  ▄▀▒   ░  ▄▀▒   ░▒██   ██░')
+    console.log('          ▄█████████▀▒  ░ ████▓▒░▒███████▒▒███████▒░ ████▓▒░')
+    console.log('              ▒░ ▒  ▒   ░ ▒ ▒░ ░░▒ ▒ ░ ▒░░▒ ▒ ░ ▒  ░ ▒ ▒░')
+    console.log('               ░          ░ ░     ▒      ░ ░  ▒     ░')
+    console.log('                           ░               ░')
+    console.log('                                                                                              ')
     console.log(nouvChn)
 }
 
