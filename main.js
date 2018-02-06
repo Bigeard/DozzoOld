8d89d1d1d84affc01cfce2aca90cac324fe99572
Bigeard <robin.bigeard@gmail.com>
2018-02-06 20:55:59 +0100

    ajout display Option, A propos, Combat

--- main.js
+++ main.js
@@ -2,13 +2,11 @@
 
 var select = 1
 
-//DISPLAY
+//DISPLAY MENU
 
 function menu(select) {
-    var page = 'menu'
     console.clear()
 
-    document.getElementById('select').innerHTML = select
     var chn = '  - Commencer [1]              - Option [2]              - A propos [3]'
    // var chn = '              - Commencer [1]              - Option [2]\n              - A propos [3]              - A propos [4]\n              - A propos [5]              - A propos [6]'
    // var chn = ' - Commencer [1]              - Option [2]              - A propos [3]\n - Commencer [4]              - Option [5]              - A propos [6]\n - Commencer [7]              - Option [8]              - A propos [9]'
@@ -24,7 +22,73 @@ function menu(select) {
     console.log('              ▒░ ▒  ▒   ░ ▒ ▒░ ░░▒ ▒ ░ ▒░░▒ ▒ ░ ▒  ░ ▒ ▒░')
     console.log('               ░          ░ ░     ▒      ░ ░  ▒     ░')
     console.log('                           ░               ░')
-    console.log('                                                                                              ')
+    console.log('                                            Menu                                            ')
     console.log(nouvChn)
 }
 
+//DISPLAY OPTION
+
+function option(select) {
+    console.clear()
+
+    var chn = '           - FR [1]                        - Difficulté [2] [▓░░░░]\n\n           - Son [3] [▓░░░░]               - Retour [4]'
+    var nouvChn = chn.replace(select, '*');
+
+    console.log('          ▀█████████▄  BINVENUE DANS LE DONJON DU ...')
+    console.log('            ██▒     █▄                                        ᚐ ᚐ ᚐ  ')
+    console.log('            ██       █▄  ▒█████  ▒███████▒▒███████▒ ▒█████')
+    console.log('           ░██       ░█ ▒██▒  ██▒▒ ▒ ▒ ▄▀░▒ ▒ ▒ ▄▀░▒██▒  ██▒')
+    console.log('           ░██      ░█▀ ▒██░  ██▒░ ▒ ▄▀▒░ ░ ▒ ▄▀▒░ ▒██░  ██▒')
+    console.log('           ▒██     ░█▀░ ▒██   ██░  ▄▀▒   ░  ▄▀▒   ░▒██   ██░')
+    console.log('          ▄█████████▀▒  ░ ████▓▒░▒███████▒▒███████▒░ ████▓▒░')
+    console.log('              ▒░ ▒  ▒   ░ ▒ ▒░ ░░▒ ▒ ░ ▒░░▒ ▒ ░ ▒  ░ ▒ ▒░')
+    console.log('               ░          ░ ░     ▒      ░ ░  ▒     ░')
+    console.log('                           ░               ░')
+    console.log('                                           Option                                           ')
+    console.log(nouvChn)
+}
+
+//DISPLAY A PROPOS
+
+function propos(select) {
+    console.clear()
+
+    var chn = '                                Retour [1]                                '
+    var nouvChn = chn.replace(select, '*');
+
+    console.log('          ▀█████████▄  BINVENUE DANS LE DONJON DU ...')
+    console.log('            ██▒     █▄                                        ᚐ ᚐ ᚐ  ')
+    console.log('            ██       █▄  ▒█████  ▒███████▒▒███████▒ ▒█████')
+    console.log('           ░██       ░█ ▒██▒  ██▒▒ ▒ ▒ ▄▀░▒ ▒ ▒ ▄▀░▒██▒  ██▒')
+    console.log('           ░██      ░█▀ ▒██░  ██▒░ ▒ ▄▀▒░ ░ ▒ ▄▀▒░ ▒██░  ██▒')
+    console.log('           ▒██     ░█▀░ ▒██   ██░  ▄▀▒   ░  ▄▀▒   ░▒██   ██░')
+    console.log('          ▄█████████▀▒  ░ ████▓▒░▒███████▒▒███████▒░ ████▓▒░')
+    console.log('              ▒░ ▒  ▒   ░ ▒ ▒░ ░░▒ ▒ ░ ▒░░▒ ▒ ░ ▒  ░ ▒ ▒░')
+    console.log('               ░          ░ ░     ▒      ░ ░  ▒     ░')
+    console.log('                           ░               ░')
+    console.log('                                         A Propos                                           ')
+    console.log('\n             Crée par Samy VERA, Robin BIGEARD, Olivier CROCHET         \n\n')
+    console.log(nouvChn)
+}
+
+function combat(select) {
+    console.clear()
+
+    var chn = '          - Attaque [1]                        - Concentration [2]\n          - Inventaire [3]                     - Capacité [X]'
+    var nouvChn = chn.replace(select, '*');
+    var msg = '                               * Slection *                                  '
+
+    console.log('[▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓]PV[▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓]')
+    console.log('')
+    console.log('')
+    console.log('             [ Bigeard ]                           [ Zombie ]             ')
+    console.log('               ______                                 _____               ')
+    console.log('              ( ͡° ͜ʖ ͡°)/                             \\(✖╭╮✖)              ')
+    console.log('                                                                             ')
+    console.log('               /    \\                                 /    \\              ')
+    console.log('')
+    console.log('')
+    console.log('                                         A Propos                                           ')
+    console.log(msg)
+    console.log(nouvChn)
+}
