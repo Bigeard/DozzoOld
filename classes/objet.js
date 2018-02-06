78e1b2a6ce6d165b20b72d5885d0ace262ee5af7
Crochet Olivier <olivier.crochet@ynov.com>
2018-02-06 19:23:49 +0100

    modification de la classe objet -> bonne syntaxe

--- classes/objet.js
+++ classes/objet.js
@@ -1,32 +1,27 @@
-class objet{
-
-   constructor(nom, effet, quantite) {
-       this.nom = nom
-       this.effet = effet
-       this.quantite = quantite
-
-   }
-
-   get nom(nom){
-       return this.nom
-   }
-
-    get effet(effet){
-        return this.effet
-    }
-    get quantite(quantite){
-        return this.quantite
-    }
-
-    set nom(nom){
-        return this.nom
-    }
-    set effet(effet){
-        return this.effet
-    }
-    set quantite(quantite){
-        return this.quantite
-    }
-
-   
+function Objet (nom, effet, description) {
+    this.nom = nom
+    this.effet = effet
+    this.description = description
 }
+
+
+
+var baton = new Objet("baton du vide", "effet", "un baton ordinaire")
+
+var potionDeSoin = new Objet("potion de soin", "effet", "potion qui rend 10 PV")
+
+
+var inventaire = {
+
+ajouterObjet : function() {
+    return 0
+},
+
+ouvrir : function (baton, potionDeSoin) {
+console.log(Objet.nom + " x" + baton.quantite)
+
+console.log(potionDeSoin.nom + " x" + potionDeSoin.quantite)
+}
+}
+var inventaire = Object.create(inventaire)
+inventaire.ouvrir(baton, potionDeSoin)
