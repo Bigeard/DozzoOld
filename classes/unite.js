16e61e5fe42a6b01d8d941d8ff3ad90b6777f45e
Shaggy <samy_leni@hotmail.fr>
2018-02-06 10:22:38 +0100

    set classe

--- classes/unite.js
+++ classes/unite.js
@@ -20,69 +20,13 @@ class Unite = {
 		this.competance = competance;
 	}
 
-	//Classes
-	public setMage() {
-		this.classe = "Mage";
-
-		this.sante += 30;
-		this.mana += 25;
-		this.attaque += 1;
-		this.magie += 8;
-		this.defense += 2;
-		this.resistance += 4;
-		this.competance = "Incantation";
-	}
-	public setGuerrier() {
-		this.classe = "Guerrier";
-
-		this.sante += 50;
-		this.mana += 5;
-		this.attaque += 10;
-		this.magie += 0;
-		this.defense += 5;
-		this.resistance += 1;
-		this.competance = "Rage";
-	}
-	public setAssassin() {
-		this.classe = "Assassin";
-
-		this.sante += 30;
-		this.mana += 10;
-		this.attaque += 15;
-		this.magie += 5;
-		this.defense += 2;
-		this.resistance += 2;
-		this.competance = "Camouflage";
-	}
-	public setAbomination() {
-		this.classe = "Abomination";
-
-		this.sante += 50;
-		this.mana += 0;
-		this.attaque += 12;
-		this.magie += 0;
-		this.defense += 7;
-		this.resistance += 4;
-		this.competance = "Empoisonnement";
-	}
-	public setMageNoir() {
-		this.classe = "Mage noir";
-
-		this.sante += 40;
-		this.mana += 30;
-		this.attaque += 0;
-		this.magie += 10;
-		this.defense += 5;
-		this.resistance += 8;
-		this.competance = "Entrave";
-	}
-
 	public get nom(nom) {
 		return this.nom;
 	}
 	public set nom(nom) {
 		this.nom = nom;
 	}
+
 
 	public get niveau(niveau) {
 		return this.niveau;
@@ -91,12 +35,14 @@ class Unite = {
 		this.niveau = niveau;
 	}
 
+
 	public get experience(experience) {
 		return this.experience;
 	}
 	public set experience(experience) {
 		this.experience = experience;
 	}
+
 
 	public get concentration(concentration) {
 		return this.concentration;
@@ -105,12 +51,68 @@ class Unite = {
 		this.concentration = concentration;
 	}
 
+
 	public get classe(classe) {
 		return this.classe;
 	}
 	public set classe(classe) {
-		this.classe = classe;
+		if (classe === "Mage") {
+		this.classe = "Mage"
+
+		this.sante += 30;
+		this.mana += 25;
+		this.attaque += 1;
+		this.magie += 8;
+		this.defense += 2;
+		this.resistance += 4;
+		this.competance = "Incantation";
+		}
+		else if (classe === "Guerrier") {
+		this.classe = "Guerrier"
+
+		this.sante += 50;
+		this.mana += 5;
+		this.attaque += 10;
+		this.magie += 0;
+		this.defense += 5;
+		this.resistance += 1;
+		this.competance = "Rage";
+		}
+		else if (classe === "Assassin") {
+		this.classe = "Assassin"
+
+		this.sante += 30;
+		this.mana += 10;
+		this.attaque += 15;
+		this.magie += 5;
+		this.defense += 2;
+		this.resistance += 2;
+		this.competance = "Camouflage";
+		}
+		else if (classe === "Abomination") {
+		this.classe = "Abomination"
+
+		this.sante += 50;
+		this.mana += 0;
+		this.attaque += 12;
+		this.magie += 0;
+		this.defense += 7;
+		this.resistance += 4;
+		this.competance = "Empoisonnement";
+		}
+		else if (classe === "Mage noir") {
+		this.classe = "Mage noir"
+
+		this.sante += 40;
+		this.mana += 30;
+		this.attaque += 0;
+		this.magie += 10;
+		this.defense += 5;
+		this.resistance += 8;
+		this.competance = "Entrave";
+		}
 	}
+
 
 	public get sante(sante) {
 		return this.sante;
@@ -119,12 +121,14 @@ class Unite = {
 		this.sante = sante;
 	}
 
+
 	public get mana(mana) {
 		return this.mana;
 	}
 	public set mana(mana) {
 		this.mana = mana;
 	}
+
 
 	public get attaque(attaque) {
 		return this.attaque;
@@ -133,12 +137,14 @@ class Unite = {
 		this.attaque = attaque;
 	}
 
+
 	public get magie(magie) {
 		return this.magie;
 	}
 	public set magie(magie) {
 		this.magie = magie;
 	}
+
 
 	public get defense(defense) {
 		return this.defense;
@@ -147,12 +153,14 @@ class Unite = {
 		this.defense = defense;
 	}
 
+
 	public get resistance(resistance) {
 		return this.resistance;
 	}
 	public set resistance(resistance) {
 		this.resistance = resistance;
 	}
+
 
 	public get competance(competance) {
 		return this.competance;
