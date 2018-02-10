17b0e00e55c3a840754236441f6dfcf2f80111c3
Shaggy <samy_leni@hotmail.fr>
2018-02-11 00:05:10 +0100

    Fusion des deux versions

--- classes/selection.js
+++ classes/selection.js
@@ -1,6 +1,6 @@
 var Selection = {
 
-    init: function (nom, nombreDeSlot, Slot1, Slot2, Slot3, Slot4, Slot5, Slot6) {
+    init: function (nom, nombreDeSlot, Slot1, Slot2, Slot3, Slot4, Slot5, Slot6, LastSlot) {
 
         //Identification
         this.nom = nom;
@@ -11,6 +11,7 @@ var Selection = {
         this.Slot4 = Slot4;
         this.Slot5 = Slot5;
         this.Slot6 = Slot6;
+        this.LastSlot = LastSlot;
     },
 
     //Identification
@@ -29,6 +30,7 @@ var Selection = {
             this.Slot1 = "Commencer"
             this.Slot2 = "Option"
             this.Slot3 = "A propos"
+            this.LastSlot = "Menu"
         }
         else if (page === "Commencer") {
             this.nom = "Combat"
@@ -37,6 +39,7 @@ var Selection = {
             this.Slot2 = "Concentration"
             this.Slot3 = "Capaciter"
             this.Slot4 = "Inventaire"
+            this.LastSlot = "Commencer"
         }
         else if (page === "Option") {
             this.nom = "Option"
@@ -44,43 +47,53 @@ var Selection = {
             this.Slot1 = "Difficulter"
             this.Slot2 = "Son"
             this.Slot3 = "Retour"
+            this.LastSlot = "Menu"
         }
         else if (page === "A propos") {
             this.nom = "A propos"
             this.nombreDeSlot = 1
             this.Slot1 = "Retour"
+            this.LastSlot = "Menu"
         }
         else if (page === "Attaque") {
             this.nom = "Attaque"
-            this.nombreDeSlot = 5
+            this.nombreDeSlot = 6
             this.Slot1 = "Attaque"
             this.Slot2 = "Attaque"
             this.Slot3 = "Attaque"
             this.Slot4 = "Attaque"
             this.Slot5 = "Attaque"
-            this.Slot6 = "Attaque"
+            this.Slot6 = "Retour"
+            this.LastSlot = "Commencer"
         }
         else if (page === "Inventaire") {
             this.nom = "Inventaire"
-            this.nombreDeSlot = 2
+            this.nombreDeSlot = 3
             this.Slot1 = "Equipement"
             this.Slot2 = "Objet"
+            this.Slot3 = "Retour"
+            this.LastSlot = "Commencer"
         }
         else if (page === "Equipement") {
             this.nom = "Equipement"
-            this.nombreDeSlot = 4
+            this.nombreDeSlot = 5
             this.Slot1 = "Equipement"
             this.Slot2 = "Equipement"
             this.Slot3 = "Equipement"
             this.Slot4 = "Equipement"
+            this.Slot5 = "Retour"
+            this.LastSlot = "Inventaire"
         }
         else if (page === "Objet") {
             this.nom = "Objet"
-            this.nombreDeSlot = 4
+            this.nombreDeSlot = 5
             this.Slot1 = "Objet1"
             this.Slot2 = "Objet2"
             this.Slot3 = "Objet3"
             this.Slot4 = "Objet4"
+            this.Slot5 = "Retour"
+            this.LastSlot = "Inventaire"
+
         }
     }
 }
--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 //INITIALISATION DU JEU
 
 var currentPage = Object.create(Selection)
-    currentPage.init(undefined, 0, undefined, undefined, undefined, undefined, undefined, undefined)
+    currentPage.init(undefined, 0, undefined, undefined, undefined, undefined, undefined, undefined, undefined)
 var currentSlotName = undefined;
 
 var positionCurseur = 1;
 
 var page = "Menu";
-var pageRetour = "Menu";
 currentPage.setPage(page)
 
 //MAIN
+
+function main() {
+    pageAffichageCurseur(1);
+}
 
 function pageAffichageCurseur(positionCurseur) {
 
@@ -30,83 +33,85 @@ function pageAffichageCurseur(positionCurseur) {
     }
     var affichagePageAvecCurseur = affichagePage.replace(positionCurseur, '*');
 
-    //3 ON CHOISI LA PAGE A AFFICHER                                             (a refaire avec une fonction et des if)
+    //3 ON CHOISI LA PAGE A AFFICHER
 
-    if (page == "Commencer") {
-        affichageCombat(affichagePageAvecCurseur)
-    }
-    else if (page == "Menu") {
-        affichageMenu(affichagePageAvecCurseur)
-    }
-    else {
-        console.clear()
-        console.log('Affichage inexistant')
-        console.log(affichagePageAvecCurseur)
-    }
+    affichage(affichagePageAvecCurseur);
 
     //AFFICHAGE DE LA POSITION DU CURSEUR DANS LE HTML
 
     document.getElementById('select').innerHTML = "[posCurseur : " +positionCurseur + "] [nomPage : " + currentPage.nom + "] [nonSlot : " + currentSlotName + "]";
     return currentSlotName
-} 
+}
 
 function pageSelected(currentSlotName) {
 
     //1 ON PREPARE LA PROCHAINE PAGE A AFFICHER
 
     if (currentSlotName == "Retour") {
-        page = pageRetour
+        page = currentPage.LastSlot;
     }
     else {
-        pageRetour = page
         page = currentSlotName
     }
 
     currentPage.setPage(page)
+    positionCurseur = 1;
 }
 
 //AFFICHAGE TYPE
 
-function affichageMenu(affichagePageAvecCurseur) {
-
+function affichage(affichagePageAvecCurseur) {
+    console.log("\n\n")
     console.clear()
-    console.log('          ▀█████████▄  BINVENUE DANS LE DONJON DU ...')
-    console.log('            ██▒     █▄                                        ᚐ ᚐ ᚐ  ')
-    console.log('            ██       █▄  ▒█████  ▒███████▒▒███████▒ ▒█████')
-    console.log('           ░██       ░█ ▒██▒  ██▒▒ ▒ ▒ ▄▀░▒ ▒ ▒ ▄▀░▒██▒  ██▒')
-    console.log('           ░██      ░█▀ ▒██░  ██▒░ ▒ ▄▀▒░ ░ ▒ ▄▀▒░ ▒██░  ██▒')
-    console.log('           ▒██     ░█▀░ ▒██   ██░  ▄▀▒   ░  ▄▀▒   ░▒██   ██░')
-    console.log('          ▄█████████▀▒  ░ ████▓▒░▒███████▒▒███████▒░ ████▓▒░')
-    console.log('              ▒░ ▒  ▒   ░ ▒ ▒░ ░░▒ ▒ ░ ▒░░▒ ▒ ░ ▒  ░ ▒ ▒░')
-    console.log('               ░          ░ ░     ▒      ░ ░  ▒     ░')
-    console.log('                           ░               ░')
-    console.log('                                             ' + currentPage.nom + '                                             ')
-    console.log()
-    console.log(affichagePageAvecCurseur)
-}
 
-function affichageCombat(affichagePageAvecCurseur) {
+    if (page == "Menu") {
 
-    console.clear()
-    console.log('\n\n\n')
-    console.log('[▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓]PV[▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓]')
-    console.log('')
-    console.log('')
-    console.log('             [ Bigeard ]                           [ Zombie ]             ')
-    console.log('               ______                                 _____               ')
-    console.log('              ( ͡° ͜ʖ ͡°)/                             \\(✖╭╮✖)              ')
-    console.log('                                                                             ')
-    console.log('               /    \\                                 /    \\              ')
-    console.log('')
-    console.log('')
-    console.log('                                             ' + currentPage.nom + '                                             ')
-    console.log()
-    console.log(affichagePageAvecCurseur)
+        /* Cette fonction permet d"afficher dans la console
+        la page de Menu */
+
+        console.log("          ▀█████████▄  BINVENUE DANS LE DONJON DU ...")
+        console.log("            ██▒     █▄                                        ᚐ ᚐ ᚐ  ")
+        console.log("            ██       █▄  ▒█████  ▒███████▒▒███████▒ ▒█████")
+        console.log("           ░██       ░█ ▒██▒  ██▒▒ ▒ ▒ ▄▀░▒ ▒ ▒ ▄▀░▒██▒  ██▒")
+        console.log("           ░██      ░█▀ ▒██░  ██▒░ ▒ ▄▀▒░ ░ ▒ ▄▀▒░ ▒██░  ██▒")
+        console.log("           ▒██     ░█▀░ ▒██   ██░  ▄▀▒   ░  ▄▀▒   ░▒██   ██░")
+        console.log("          ▄█████████▀▒  ░ ████▓▒░▒███████▒▒███████▒░ ████▓▒░")
+        console.log("              ▒░ ▒  ▒   ░ ▒ ▒░ ░░▒ ▒ ░ ▒░░▒ ▒ ░ ▒  ░ ▒ ▒░")
+        console.log("               ░          ░ ░     ▒      ░ ░  ▒     ░")
+        console.log("                           ░               ░")
+        console.log("                                             " + currentPage.nom + "                                             ")
+        console.log()
+        console.log(affichagePageAvecCurseur)
+    }
+    else if (page == "Commencer") {
+
+        /* Cette fonction permet de d"afficher dans la console
+        la scène de combat en fonction des PV de l'héro, du monstre,
+        du nom de la sélection de la page, sélection disponible, etc... */
+
+        console.log("[▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓]PV[▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓]")
+        console.log("")
+        console.log("")
+        console.log("             [ Bigeard ]                           [ Zombie ]             ")
+        console.log("               ______                                 _____               ")
+        console.log("              ( ͡° ͜ʖ ͡°)/                             \\(✖╭╮✖)              ")
+        console.log("                                                                             ")
+        console.log("               /    \\                                 /    \\              ")
+        console.log("")
+        console.log("")
+        console.log("                                             " + currentPage.nom + "                                             ")
+        console.log()
+        console.log(affichagePageAvecCurseur)
+    }
+    else {
+        console.log('Affichage inexistant')
+        console.log(affichagePageAvecCurseur)
+    }
 }
 
 //HTML PAGE CLASSE
 
-function chowClasse() {
+function affichageHero() {
 
     var chowStats = Object.create(Unite)
     var changeClasse = ["Guerrier", "Mage", "Assassin", "Mage noir"]
