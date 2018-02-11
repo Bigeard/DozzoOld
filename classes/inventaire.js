5c7c8172b7d8be39fa04dbbd775ecb50e25dd7a4
Crochet Olivier <olivier.crochet@ynov.com>
2018-02-11 20:44:13 +0100

    affichage inventraire et ajout d'objet fonctionnel

--- classes/inventaire.js
+++ classes/inventaire.js
@@ -1,13 +1,5 @@
 function Inventaire(Objet) {
     this.Objet = Objet
-
-    function OuvrirInventaire() {
-        console.log(InventaireJoueur.Objet)
-    }
-
-    function AjouterObjet(Objet){
-        InventaireJoueur += Objet
-    }
 }
 
 Inventaire.prototype = Object.create(Inventaire.prototype)
@@ -16,7 +8,21 @@ Inventaire.prototype.constructor = Inventaire
 
 var InventaireJoueur = new Inventaire([PotionDeSoin, Rapiere])
 
-InventaireJoueur.OuvrirInventaire()
+
+function OuvrirInventaire() {
+    
+    for(i = 0; i < InventaireJoueur.Objet.length; i++) {
+        console.log("["+ (i+1) +"] " + InventaireJoueur.Objet[i].nom + ", " + InventaireJoueur.Objet[i].description)
+    }
+}
+
+function AjouterObjet(Inventaire, Objet){
+    InventaireJoueur.Objet[InventaireJoueur.Objet.length ++] = Objet
+}
+
+OuvrirInventaire()
+AjouterObjet(InventaireJoueur, ganteletDeFer)
+OuvrirInventaire()
     
 
 
