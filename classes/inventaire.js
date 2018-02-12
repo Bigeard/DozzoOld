48236475da6bf0c6651769d05dfbb8f6f1b0c318
Crochet Olivier <olivier.crochet@ynov.com>
2018-02-12 10:39:27 +0100

    léger clean du code

--- classes/inventaire.js
+++ classes/inventaire.js
@@ -1,35 +1,23 @@
 function Inventaire(Objet) {
-    this.Objet = Objet
+    this.Objet = Objet;
 }
 
-Inventaire.prototype = Object.create(Inventaire.prototype)
+Inventaire.prototype = Object.create(Inventaire.prototype);
 
-Inventaire.prototype.constructor = Inventaire
+Inventaire.prototype.constructor = Inventaire;
 
-var InventaireJoueur = new Inventaire([PotionDeSoin, Rapiere])
+var InventaireJoueur = new Inventaire([PotionDeSoin, Rapiere]);
 
 
 function OuvrirInventaire() {
     
     for(i = 0; i < InventaireJoueur.Objet.length; i++) {
-        console.log("["+ (i+1) +"] " + InventaireJoueur.Objet[i].nom + ", " + InventaireJoueur.Objet[i].description)
+        console.log("["+ (i+1) +"] " + InventaireJoueur.Objet[i].nom + ", " + InventaireJoueur.Objet[i].description);
     }
 }
 
 function AjouterObjet(Inventaire, Objet){
-    InventaireJoueur.Objet[InventaireJoueur.Objet.length ++] = Objet
+    InventaireJoueur.Objet[InventaireJoueur.Objet.length ++] = Objet;
 }
 
-OuvrirInventaire()
-AjouterObjet(InventaireJoueur, ganteletDeFer)
-OuvrirInventaire()
     
-
-
-/*if (ganteletDeFer.estEquipe == true){
-    console.log(ganteletDeFer.nom + ", " + ganteletDeFer.description)
-    console.warn("equipé")
-    }
-    else{
-    console.log(ganteletDeFer.nom + ", " + ganteletDeFer.description)
-    }*/
--- classes/objet.js
+++ classes/objet.js
@@ -10,7 +10,7 @@ magieModif, defenseModif, resistanceModif, effet, description, quantite) {
 
     Objet.call(this, nom, ptsVieModif, ptsManaModif, attaqueModif, magieModif, defenseModif, resistanceModif,effet, description)
 
-    this.quantite = quantite
+    this.quantite = quantite;
 }
 
 function Equipement (nom, ptsVieModif, ptsManaModif, attaqueModif, magieModif, 
@@ -19,7 +19,7 @@ defenseModif, resistanceModif, effet, description, typeEquipement) {
     Objet.call(this, nom, ptsVieModif, ptsManaModif, attaqueModif,
         magieModif, defenseModif, resistanceModif, effet, description)
 
-    this.typeEquipement = typeEquipement
+    this.typeEquipement = typeEquipement;
 }
 
 function Armure (nom, ptsVieModif, ptsManaModif, attaqueModif, magieModif,
@@ -28,17 +28,17 @@ defenseModif, resistanceModif,effet, description, typeEquipement, estEquipe){
     Equipement.call(this, nom, ptsVieModif, ptsManaModif, attaqueModif, magieModif,
     defenseModif, resistanceModif, effet, description, typeEquipement)
 
-    this.estEquipe = estEquipe
+    this.estEquipe = estEquipe;
 }
 
-Consommable.prototype = Object.create(Objet.prototype)
+Consommable.prototype = Object.create(Objet.prototype);
 
-Consommable.prototype.constructor = Consommable
-var Rapiere = new Equipement("Rapière", 0, 0, 4, 0, 0, 0, "effet", "épée longue et fine", "épée")
+Consommable.prototype.constructor = Consommable;
+var Rapiere = new Equipement("Rapière", 0, 0, 4, 0, 0, 0, "effet", "épée longue et fine", "épée");
 
-var PotionDeSoin = new Consommable("Potion de soin", 10, 0, 0, 0, 0, 0, "effet", "Potion qui soigne 10PV", 2)
+var PotionDeSoin = new Consommable("Potion de soin", 10, 0, 0, 0, 0, 0, "effet", "Potion qui soigne 10PV", 2);
 
-var ganteletDeFer = new Armure("Gantelet de fer", 0, 0, 0, 0, 2, 0, "effet", "protection pour les poignets", "gant", true)
+var ganteletDeFer = new Armure("Gantelet de fer", 0, 0, 0, 0, 2, 0, "effet", "protection pour les poignets", "gant", true);
  
 /*var Objet = {
 
