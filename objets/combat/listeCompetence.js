1e988ca7dba21620e759939f85c66b891357beaf
Bigeard <robin.bigeard@gmail.com>
2018-02-25 15:18:52 +0100

    ajout de la posibiliter d'utiliser le competance

--- objets/combat/listeCompetence.js
+++ objets/combat/listeCompetence.js
@@ -1,36 +1,49 @@
 //LISTE DES COMPETENCES
 
 var competenceRage = {
-	nom: 'Rage',
-    effet: function() {
-    	//INSTRUCTION
+    nom: 'Rage',
+    quantiteDozzo: 3,
+    attaque: 15,
+    effet: function () {
+        if (uniteTour.ptsDozzo === this.quantiteDozzo) {
+            uniteCible.ptsVie -= this.attaque;
+            uniteTour.ptsDozzo -= this.quantiteDozzo+1;
+            changerUnite();
+        }
+        else {
+            //INSTRUCTION
+        }
     }
 }
 
 var competenceIncantation = {
-	nom: 'Incantation',
-    effet: function() {
-    	//INSTRUCTION
+    nom: 'Incantation',
+    quantiteDozzo: 3,
+    effet: function () {
+        //INSTRUCTION
     }
 }
 
 var competenceCamouflage = {
-	nom: 'Camouflage',
-    effet: function() {
-    	//INSTRUCTION
+    nom: 'Camouflage',
+    quantiteDozzo: 3,
+    effet: function () {
+        //INSTRUCTION
     }
 }
 
 var competenceEntrave = {
-	nom: 'Entrave',
-    effet: function() {
-    	//INSTRUCTION
+    nom: 'Entrave',
+    quantiteDozzo: 3,
+    effet: function () {
+        //INSTRUCTION
     }
 }
 
 var competenceEmpoisonnement = {
-	nom: 'Empoisonnement',
-    effet: function() {
-    	//INSTRUCTION
+    nom: 'Empoisonnement',
+    quantiteDozzo: 2,
+    effet: function () {
+        //INSTRUCTION
     }
 }
